990e4bfdd9750d3f6531a1ade0bcdb81bd31b9c0
Rishik1507 <microsoftrishik@gmail.com>
2024-12-12 17:19:22 +0530

    c

---
+++ acp.py
@@ -0,0 +1,11 @@
+class obj():
+    def __init__(self,num1,num2,num3):
+        self.num1=num1
+        self.num2=num2
+        self.num3=num3
+    def add(self):
+
+        print(f'The result is {self.num1+self.num2+self.num3}')
+o=obj(344,55,4)
+o.add()
+        
